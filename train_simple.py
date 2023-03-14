6441e1f3b973616cd089a1409c81327b784d5708
Levchenko Nikita <n.levchenko@everypixel.ru>
2023-03-14 14:05:13 +0500

    100001-ая итерация

--- train_simple.py
+++ train_simple.py
@@ -607,7 +607,7 @@ if __name__ == '__main__':
 
 
     # parser.add_argument('--pretrain', type=str, default=None)
-    parser.add_argument('--pretrain', type=str, default='ckpts/050000.pth')
+    parser.add_argument('--pretrain', type=str, default='ckpts/100000.pth')
     # parser.add_argument('--pretrain', type=str, default=None)
 
 
@@ -662,7 +662,7 @@ if __name__ == '__main__':
 
     # Стартовая итерация
     # Видимо, для пауз процесса обучения
-    args.start_iter = 50001
+    args.start_iter = 100001
 
 
 
