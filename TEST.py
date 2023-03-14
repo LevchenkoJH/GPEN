aaa3d886159ca02776c7c677b056bd3be6f3186c
Levchenko Nikita <n.levchenko@everypixel.ru>
2023-03-14 14:46:22 +0500

    100001-ая итерация

--- TEST.py
+++ TEST.py
@@ -1,31 +1,31 @@
 #########################################################################################################################
-# # Для подготовки датасета
-# import os
-# import glob
-#
-#
+# Для подготовки датасета
+import os
+import glob
+
+
 # path = '/home/jasmine/VGGFace2-HQ/Data/VGGface2_FFHQalign'
-# # path = '/home/levchenkone/GITHUB/GPEN/examples/test_dataset'
-#
-# HQ_imgs = glob.glob(os.path.join(path, '*', '*coef*'))
-#
-# # print(len(HQ_imgs))
-#
-#
-# count = 0
-# for p in HQ_imgs:
-#     images_path = glob.glob(os.path.join(p, '*.*'))
-#     # print(len(images_path))
-#     if (len(images_path) == 0):
-#         print(p)
-#         os.rmdir(p)
-#         count +=1
-#     if (len(images_path) == 1):
-#         print(p)
-#         os.remove(images_path[0])
-#         count += 1
-#
-# print("count =", count)
+path = '/home/levchenkone/GITHUB/GPEN/examples/test_dataset'
+
+HQ_imgs = glob.glob(os.path.join(path, '*', '*coef*'))
+
+# print(len(HQ_imgs))
+
+
+count = 0
+for p in HQ_imgs:
+    images_path = glob.glob(os.path.join(p, '*.*'))
+    # print(len(images_path))
+    if (len(images_path) == 0):
+        print(p)
+        os.rmdir(p)
+        count +=1
+    if (len(images_path) == 1):
+        print(p)
+        os.remove(images_path[0])
+        count += 1
+
+print("count =", count)
 #########################################################################################################################
 # # Хотим переместить датасет
 # import os
