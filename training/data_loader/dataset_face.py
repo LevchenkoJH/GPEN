a43a13d6bb5bea2ef2aa8665c57831316a8be771
jasmine <konzernevl@mail.ru>
2023-05-15 00:00:42 +0500

    батч 24

--- training/data_loader/dataset_face.py
+++ training/data_loader/dataset_face.py
@@ -90,6 +90,7 @@ class GFPGAN_degradation(object):
 
 class FaceDataset(Dataset):
     def __init__(self, path, resolution=512):
+        path = r'/home/levchenkone/GITHUB/GPEN/examples/test_dataset/*/*'
         print("--------------------------------------------INIT FaceDataset-------------------------------------------", flush=True)
         self.resolution = resolution
         print("resolution =", resolution, flush=True)
