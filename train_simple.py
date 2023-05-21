ebbdd7ad55feab2b50502226a8290a4db5a2a7b3
Levchenko Nikita <70745236+LevchenkoJH@users.noreply.github.com>
2023-05-21 18:32:16 +0500

    с нуля из-за глупости

--- train_simple.py
+++ train_simple.py
@@ -599,7 +599,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--local_rank', type=int, default=0)
     # ckpt weights
-    parser.add_argument('--ckpt', type=str, default='ckpts')
+    parser.add_argument('--ckpt', type=str, default='ckpts-2')
 
 
 
@@ -610,8 +610,8 @@ if __name__ == '__main__':
 
 
     # parser.add_argument('--pretrain', type=str, default=None)
-    parser.add_argument('--pretrain', type=str, default='ckpts/045000.pth')
-    # parser.add_argument('--pretrain', type=str, default=None)
+    # parser.add_argument('--pretrain', type=str, default='ckpts/045000.pth')
+    parser.add_argument('--pretrain', type=str, default=None)
 
 
 
@@ -667,7 +667,7 @@ if __name__ == '__main__':
     # Стартовая итерация
     # Видимо, для пауз процесса обучения
     # args.start_iter = 370001
-    args.start_iter = 45001
+    args.start_iter = 0
 
 
 
